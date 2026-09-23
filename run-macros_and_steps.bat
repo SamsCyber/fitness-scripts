bf967261a211b27bcc688578f5e830b02a705beb
sfc <samfredc@gmail.com>
2026-09-23 20:21:23 +0100

    fix ps file name in batch

--- run-macros_and_steps.bat
+++ run-macros_and_steps.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0macros_and_steps.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0run-macros_and_steps.ps1"
 pause
